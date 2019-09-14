7e9330b72aeed8d37399cda7793c6d6172cf746d
Paulo Victor <42791144+paulov59@users.noreply.github.com>
2019-09-13 22:24:30 -0300

    Update multiplayer.py

--- multiplayer.py
+++ multiplayer.py
@@ -1,9 +1,11 @@
+import os
+
 global matriz
 matriz = [[' ', ' ', ' '], [' ', ' ', ' '], [' ', ' ', ' ']]
 
 def posicoes():
     aux = 1
-    print("\tPosicoes do jogo:\n")
+    print("Posicoes do jogo:\n")
     for i in range(0,3):
         print("\t", end='')
         for j in range(0,3):
@@ -68,43 +70,53 @@ def verificar():
 
     for i in range(0,3):
         if matriz[0][i] == matriz[1][i] == matriz[2][i]:
-            return matriz[0][i];
+            return matriz[0][i]
 
     if matriz[0][0] == matriz[1][1] == matriz[2][2]:
-        return matriz[0][0];
+        return matriz[0][0]
 
     if matriz[0][2] == matriz[1][1] == matriz[2][0]:
-        return matriz[0][2];
+        return matriz[0][2]
 
-    return ' ';
+    return ' '
+
+def velha():
+    for i in range(0,3):
+        for j in range(0,3):
+            if matriz[i][j] == ' ':
+                return False
+    return True
 
 def main():
     print("Bem vindo(a) ao Jogo da Velha da Oficina de Teoria dos Jogos!")
     check = ' '
-    jogadas = 1
-    posicoes()
-    
-    while jogadas <= 9:
-        mapa()
-        pos = int(input("Digite o valor de uma posicao no tabuleiro: "))
-        jogada(pos, 'X')
-        print()
-        check = verificar()
-        if check != ' ':
-            break
+  
+    while check == ' ':
+        posicoes()
         mapa()
         pos = int(input("Digite o valor de uma posicao no tabuleiro: "))
         jogada(pos, 'O')
         check = verificar()
         if check != ' ':
             break
-        jogadas += 1
+        if velha() == True:
+            break
+        os.system('cls' if os.name == 'nt' else 'clear')
+        posicoes()
+        mapa()
+        pos = int(input("Digite o valor de uma posicao no tabuleiro: "))
+        jogada(pos, 'X')
+        check = verificar()
+        if velha() == True:
+            break
+        os.system('cls' if os.name == 'nt' else 'clear')
     
+    os.system('cls' if os.name == 'nt' else 'clear')
     mapa()
 
-    if check == 'X':
+    if check == 'O':
         print("\tJogador 1 venceu!!\n")
-    elif check == 'O':
+    elif check == 'X':
         print("\tJogador 2 venceu!!\n")
     else:
         print("\tDeu velha!!\n")
